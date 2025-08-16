b6b88846ff93d4820dd1ad1097fc6aac8d3657e6
Ahmad-hsn1 <123625145+Ahmad-hsn1@users.noreply.github.com>
2025-08-16 14:55:28 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -37,7 +37,7 @@ export const HeroSection = () => {
         </div>
 
         {/* Image Section */}
-      <div className="md:block animate-fade-in-delay-4 md:w-1/2 card-hover left-60">
+      <div className=" animate-fade-in-delay-4 md:w-1/2 card-hover left-60">
         <img 
           src={yourImage} 
           alt="Ahmad Hassan Khalid" 
