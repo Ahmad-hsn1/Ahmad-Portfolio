da99d3c201b84de71cffd74e1695128202998ebc
Ahmad-hsn1 <123625145+Ahmad-hsn1@users.noreply.github.com>
2025-08-16 15:17:21 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -41,7 +41,7 @@ export const HeroSection = () => {
         <img 
           src={yourImage} 
           alt="Ahmad Hassan Khalid" 
-          className="w-full h-auto" 
+          className="w-auto h-auto" 
         />
       </div>
 
