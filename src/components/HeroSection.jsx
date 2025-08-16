5878a4ab1ea16922690d1e87de9382b177ff014c
Ahmad-hsn1 <123625145+Ahmad-hsn1@users.noreply.github.com>
2025-08-16 13:23:54 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ export const HeroSection = () => {
   return (
     <section
       id="hero"
-      className="relative min-h-screen flex flex-col md:flex-row items-center justify-center px-25"
+      className="relative min-h-screen flex flex-col md:flex-row items-center justify-center "
     >
       <div className="container max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between z-10">
         
